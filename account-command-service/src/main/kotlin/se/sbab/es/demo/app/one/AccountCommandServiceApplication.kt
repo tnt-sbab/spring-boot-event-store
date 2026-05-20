e971d3dcb7a10a0c096f18aff66d1d56246c8fdf
pknu <peter.knuts@sbab.se>
2026-05-20 09:21:29 +0200

    Update dependencies and replace UrlApplicationRunner with banner files

--- account-command-service/src/main/kotlin/se/sbab/es/demo/app/one/AccountCommandServiceApplication.kt
+++ account-command-service/src/main/kotlin/se/sbab/es/demo/app/one/AccountCommandServiceApplication.kt
@@ -1,14 +1,8 @@
 package se.sbab.es.demo.app.one
 
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.ApplicationArguments
-import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
-import org.springframework.context.annotation.Profile
-import org.springframework.stereotype.Component
 import se.sbab.eventsourcing.EventsourcingConfiguration
 
 @SpringBootApplication
@@ -17,17 +11,4 @@ class AccountCommandServiceApplication
 
 fun main(args: Array<String>) {
     runApplication<AccountCommandServiceApplication>(*args)
-}
-
-@Component
-@Profile("dev")
-class UrlApplicationRunner(
-    @Value("\${server.port:8080}") private val port: String
-) : ApplicationRunner {
-    private val logger = LoggerFactory.getLogger(javaClass)
-
-    override fun run(args: ApplicationArguments) {
-        logger.info("Swagger URL: http://localhost:$port/account-command-service/swagger-ui.html")
-        logger.info("H2 URL: http://localhost:$port/account-command-service/h2-console")
-    }
 }
--- account-query-service/src/main/kotlin/se/sbab/es/demo/query/AccountQueryServiceApplication.kt
+++ account-query-service/src/main/kotlin/se/sbab/es/demo/query/AccountQueryServiceApplication.kt
@@ -1,30 +1,11 @@
 package se.sbab.es.demo.query
 
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.ApplicationArguments
-import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Profile
-import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class AccountQueryServiceApplication
 
 fun main(args: Array<String>) {
 	runApplication<AccountQueryServiceApplication>(*args)
-}
-
-@Component
-@Profile("dev")
-class UrlApplicationRunner(
-	@Value("\${server.port:8080}") private val port: String
-) : ApplicationRunner {
-	private val logger = LoggerFactory.getLogger(javaClass)
-
-	override fun run(args: ApplicationArguments) {
-		logger.info("H2 URL: http://localhost:$port/account-query-service/h2-console")
-		logger.info("GraphiQL: http://localhost:$port/account-query-service/graphiql")
-	}
 }
